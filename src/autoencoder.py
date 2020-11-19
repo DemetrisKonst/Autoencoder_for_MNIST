d78a1149e4c82d427f36666ece1fe497a6caafd8
AndrewSpano <andrewspanopoulos@gmail.com>
2020-11-19 16:51:48 +0200

    input interface finished

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,4 +1,36 @@
-from tensorflow.keras import Sequential, optimizers
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, LeakyReLU
+# from tensorflow.keras import Sequential, optimizers
+# from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, LeakyReLU
 
+import logging
 from utils import *
+
+
+
+def main(args):
+    """ main() driver function """
+
+    # first make sure that the path to the provided dataset is valid
+    if filepath_is_not_valid(args.data):
+        logging.error("The path {} is not a file. Aborting..".format(args.data))
+        exit()
+
+    # get the data from the training set
+    X = parse_dataset(args.data)
+    # y = parse_labelset("../Dataset/train-labels-idx1-ubyte")
+
+    print_image(X[0], 28, 28)
+    # print(y[0])
+
+    print_image(X[1], 28, 28)
+    # print(y[1])
+
+
+if __name__ == "__main__":
+    """ call main() function here """
+    print()
+    # configure the level of the logging and the format of the messages
+    logging.basicConfig(level=logging.ERROR, format="%(levelname)s: %(message)s\n")
+    # parse the command line input
+    args = parse_input(autoencoder=True)
+    # call the main() driver function
+    main(args)
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,15 @@
+import os
+import struct
+import logging
 import argparse
+import numpy as np
 
-
-def parse_input(arg, autoencoder=True):
+def parse_input(arg=None, autoencoder=True):
     """ function used to parse the command line input of the autoencoder """
 
     # create the argument parser
-    description = "Python script that creates an autoencoder used to reduce the dimensionality of "
-                  "the MNIST dataset."
+    description = "Python script that creates an autoencoder used to reduce the dimensionality " \
+                  "of the MNIST dataset."
     parser = argparse.ArgumentParser(description=description)
 
     # add an argument for the path of the dataset
@@ -34,3 +37,60 @@ def parse_input(arg, autoencoder=True):
 
     # parse the arguments and return the result
     return parser.parse_args(arg)
+
+
+def filepath_is_not_valid(filepath):
+    """ function used to check whether a filepath containing information is valid """
+
+    # check if the path leads to a file
+    if not os.path.isfile(filepath):
+        # return false
+        return True
+
+    # return false since the path is valid
+    return False
+
+
+def parse_dataset(filepath):
+    """ function used to parse the data of a dataset """
+
+    # open the dataset
+    with open(filepath, "rb") as dataset:
+        # read the magic number and the number of images
+        magic_number, number_of_images = struct.unpack(">II", dataset.read(8))
+        # read the number of rows and number of columns per image
+        rows, columns = struct.unpack(">II", dataset.read(8))
+        # now read the rest of the file using numpy.fromfile()
+        images = np.fromfile(dataset, dtype=np.dtype(np.uint8).newbyteorder(">"))
+        # reshape so that the final shape is (number_of_images, rows, columns)
+        images = images.reshape((number_of_images, rows, columns))
+
+    # return the images
+    return images
+
+
+def parse_labelset(filepath):
+    """ function used to parse the data of a labelset """
+
+    # open the file
+    with open(filepath, "rb") as labelset:
+        # read the magic number and the number of labels
+        magic_number, number_of_labels = struct.unpack(">II", labelset.read(8))
+        # now read the rest of the file using numpy.fromfile()
+        labels = np.fromfile(labelset, dtype=np.dtype(np.uint8).newbyteorder(">"))
+
+    # return the labels
+    return labels
+
+
+def print_image(image, rows, columns):
+    """ function used to print an image to the console """
+
+    # for each row of the image
+    for i in range(rows):
+        # for each column
+        for j in range(columns):
+            # print the value at the coordinate (i, j)
+            print("{:3d}".format(image[i, j]), end="")
+        # print a newline since the row has finished
+        print()
